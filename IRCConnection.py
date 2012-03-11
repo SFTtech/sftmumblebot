a0563bad2a28dde64e15944c838330ed0800316c
mic_e <michael@ensslin.cc>
2012-03-11 01:45:21 +0100

    changed IRCConnection.py to be based on AbstractConnection.py. pretty much re-wrote the whole file.

--- IRCConnection.py
+++ IRCConnection.py
@@ -1,164 +1,91 @@
+import AbstractConnection
 import sys
 import socket
 import string
-import thread
-import threading
-import traceback
 
-class IRCConnection:
-    # some global configuration variabels set by the constructor
-    _socket = None
-    _channel = None
+class IRCConnection(AbstractConnection):
+    # global configuration variables set by the constructor.
     _hostname = None
-    _nickname = None
     _port = None
-    _loglevel = 0
+    _nickname = None
+    _channel = None
     _encoding = None
 
-    # the mutex used when sending lines
-    _sendingLock = None
-
-    # the buffer used for reading from the socket 
-    _readbuffer = ""
-
-    # this variable stores if we're currently connected
-    _connected = False
-
-    #a list of all callback functions that will be invoked when a text message is received.
-    _textCallback = []
-    #a list of all callback functions that will be invoked when the connection is established.
-    _connectionEstablishedCallback = []
-    #a list of all callback functions that will be invoked when the connection is lost.
-    _connectionLostCallback = []
-    #a list of all callback functions that will be invoked when a connection attempt fails.
-    _connectionFailedCallback = []
-
-    def registerTextCallback(self, function):
-        self._textCallback.append(function)
-
-    def registerConnectionEstablishedCallback(self, function):
-        self._connectionEstablishedCallback.append(function)
-
-    def registerConnectionLostCallback(self, function):
-        self._connectionLostCallback.append(function)
-
-    def registerConnectionFailedCallback(self, function):
-        self._connectionFailedCallback.append(function)
-
-    def __init__(self, hostname, port, nickname, channel, encoding, loglevel):
-        self._port = port
+    # call the superconstructor and set global configuration variables.
+    def __init__(self, hostname, port, nickname, channel, encoding, name, loglevel):
+        super(IRCConnection,self).__init__(name, loglevel)
         self._hostname = hostname
-        self._channel = channel
-        self._sendingLock = threading.Lock()
-        self._loglevel = loglevel
-        self._connected = False
-        self._encoding = encoding
+        self._port = port
         self._nickname = nickname
+        self._channel = channel
+        self._encoding = encoding
 
-    def start(self):
-        thread.start_new_thread(self.run, ())
+    # the socket that will be used for communication with the IRC server.
+    _socket = None
 
-    def stop(self):
-        self._connected = False
+    # open the socket and return true. don't catch exceptions, since the run() wrapper will do that.
+    def _openConnection(self):
+        self._socket = socket.socket()
+        self._socket.connect((self._hostname, self._port))
+        return True
 
-    def run(self):
-        try:
-            self._socket = socket.socket()
-            self._socket.connect((self._hostname, self._port))
-        except:
-            self.log("couldn't establish connection to host " + self._hostname + " port " + str(self._port), 0)
-            # invoke the connectionFailed callback functions
-            for f in self._connectionFailedCallback:
-                f()
-            return
+    # send initial packages (NICKname, USER identification, channel JOIN).
+    def _initConnection(self):
+        if not self._sendMessage("NICK %s" % self._nickname):
+            raise Exception("could not send NICK message.")
+        if not self._sendMessage("USER %s %s bla :%s" % (self._nickname, self._hostname, self._nickname)):
+            raise Exception("could not send USER message.")
+        if not self._sendMessage("JOIN #%s" % self._channel):
+            raise Exception("could not send JOIN message.")
+        return True
 
-        try:
-            self._sendRawString("NICK %s" % self._nickname)
-            self._sendRawString("USER %s %s bla :%s" % (self._nickname, self._hostname, self._nickname))
-            self._sendRawString("JOIN #%s" % self._channel)
-        except:
-            self.log("couldn't send NICK/USR/JOIN messages to socket", 0)
-            # invoke the connectionFailed callback functions
-            for f in self._connectionFailedCallback:
-                f()
-            return
+    # post-connect, call _connectionEstablished() and return true.
+    def _postConnect(self):
+        self._connectionEstablished()
+        return True
 
-        # we can now consider ourselves connected
-        self._connected = True
+    # close the socket.
+    def _closeConnection(self):
+        self._socket.shutdown(socket.SHUT_RDWR)
+        self._socket.close()
+        return True
 
-        # invoke the connectionEstablished callback functions
-        for f in self._connectionEstablishedCallback:
-            f()
+    # the read buffer, which contains all currently read, but uninterpreted data.
+    _readBuffer = ""
 
-        try:
-            self.listeningLoop()
-        except:
-            self.log("exception in ircConnection listeningLoop:\n"+traceback.format_exc(), 0)
-        self._connected = False
-        
-        try:
-            self._socket.shutdown(socket.SHUT_RDWR)
-            self._socket.close()
-            self.log("socket closed.", 1)
-        except:
-            self.log("socket could not be shut down and closed:\n"+traceback.format_exc(), 1)
-        
-        # invoke the connectionLost callback functions
-        for f in self._connectionLostCallback:
-            f()
+    # read and interpret data from socket in this method.
+    def _listen(self):
+        # read up to 1 kB of data into the buffer.
+        self._readBuffer += self._socket.recv(1024)
+        # get all distinct lines from the buffer into lines.
+        lines = self._readBuffer.split('\n')
+        # move the last (unfinished) line back into the buffer.
+        self._readBuffer = lines.pop()
 
-    def log(self, message, level):
-        if(self._loglevel >= level):
-            print(message)
+        # process all lines.
+        for line in lines:
+            self._log("rx: "+line, 3)
+            # split the line up at spaces
+            line = line.rstrip().split(' ', 3)
+            
+            # check if the line contains a ping message (PING)
+            if(len(line) >= 2):
+                if(line[0] == "PING"):
+                    self._sendRawString("PONG " + line[1])
 
-    def listeningLoop(self):
-        while self._connected:
-            # read a chunk of data from socket, into the read buffer
-            self._readbuffer += self._socket.recv(1024)
+            # check if the line contains a private message (PRIVMSG)
+            if(len(line) >= 4):
+                if(line[1] == "PRIVMSG"):
+                    self._invokeTextCallback(line[0].split('!')[0].lstrip(': '), line[3].lstrip(': '))
 
-            # split the data in lines, store these in temp. leave the beginning of the last line in the read buffer
-            temp=string.split(self._readbuffer, "\n")
-            self._readbuffer=temp.pop()
+        return True
 
-            # iterate over all received lines
-            for line in temp:
-                # split the line at space characters
-                # it would be more clean to split it only until the first colon is received, but we're too lazy for that
-                self.log("rx: "+line, 3)
-                line=string.rstrip(line)
-                line=line.split(' ', 3)
-                self.log("rx+:"+str(line), 4)
+    # send the given line via the socket. don't catch any exceptions.
+    def _sendMessageUnsafe(self, message):
+        self._log("tx: " + message, 3)
+        self._socket.send(message.encode(self._encoding, errors='ignore') + "\n")
+        return True
 
-                # check if the message is a PING message. if yes, respond with PONG to stay alive
-                if(len(line) >= 2):
-                    if(line[0]=="PING"):
-                        self._sendRawString("PONG %s" % line[1])
-
-                # check if the message is a PRIVMSG message. if yes, parse it and call the appropriate callback methods
-                if(len(line) >= 4):
-                    if(line[1]=="PRIVMSG"):
-                        self._messageReceived(line[0].split('!~')[0].lstrip(': '), line[3].lstrip(': '))
-
-    def _sendRawString(self, message):
-        self._sendingLock.acquire()
-        try:
-            # synchronized gschichtn
-            self.log("tx: "+message, 2)
-            self._socket.send(message.encode(self._encoding, errors='ignore') + "\n")
-        finally:
-            self._sendingLock.release()
-   
-    def sendTextMessage(self, message):
-        if not self._connected:
-            self.log("warning: tried to send text message, but not connected", 1)
-
-        # send the message
-        self._sendRawString("PRIVMSG #" + self._channel + " :" + message)
-
-    #a list of all callback functions that will be invoked when a text message is received.
-    _textCallback = []
-
-    def _messageReceived(self, sender, text):
-        text = text.decode(self._encoding, errors='ignore')
-        for f in self._textCallback:
-            f(sender, text)
+    # pass the given line to _sendMessage, encoded as a PRIVMSG to #self._channel.
+    def _sendTextMessageUnsafe(self, message):
+        return self._sendMessage("PRIVMSG #" + self._channel + " :" + message)
